a075617e563eaa10266cd4bc1ee3e95084387100
hrs201 <haarisrahmanyar@gmail.com>
2025-09-13 16:07:00 -0400

    Initialize MERN stack blog webpage with essential files and dependencies
    - Created package.json with project metadata and dependencies
    - Added public directories for css, images, and javascript
    - Implemented main route in server with a basic "Hello World" response

---
+++ server/routes/main.js
@@ -0,0 +1,14 @@
+const express = requires('express');
+const router = express.Router();
+
+//Routes
+router.get('', (req, res) => {
+    res.send("Hello World");
+});
+
+
+module.exports = router;
+
+
+
+
